4ac9c2b8be7e26dd5e9f133ccd2ffbc5b6d730e3
Amos Marawa <tendekai.marawa.12@aberdeen.ac.uk>
2018-03-28 14:55:53 +0000

    testing

--- test/controllers/carts_controller_test.rb
+++ test/controllers/carts_controller_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class CartsControllerTest < ActionController::TestCase
+  include Devise::Test::ControllerHelpers
   test "should get show" do
     get :show
     assert_response :success
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
+ include Devise::Test::ControllerHelpers
   test "should get new" do
     get :new
     assert_response :success
